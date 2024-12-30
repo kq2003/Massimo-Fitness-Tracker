18e03fe7b255f3c21a7176ff213ecb9cccb7da3f
Ao Tian <aotian@berkeley.edu>
2024-12-30 19:09:47 +0800

    try deplou 1

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = '8bc6dbc692c5d20d516b46ad206d11fb6ab2c9ce7de2a96f878befa779754f56'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://broski_qin:Tonyqin2003@massimodb.crois6sgstj6.us-east-1.rds.amazonaws.com:5432/massimo'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # AWS S3 Configuration
-    S3_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME')
-    S3_ACCESS_KEY = os.environ.get('S3_ACCESS_KEY')
-    S3_SECRET_KEY = os.environ.get('S3_SECRET_KEY')
-    S3_REGION = os.environ.get('S3_REGION')
-    OPENAI_API_KEY= os.getenv('OPENAI_API_KEY')
+    S3_BUCKET_NAME = 'massimo-gym-videos'
+    S3_ACCESS_KEY = 'AKIAVPEYV6NA7JJ3SMJO'
+    S3_SECRET_KEY = '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'
+    S3_REGION = 'us-east-1'
+    OPENAI_API_KEY= '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'
 
 
 
