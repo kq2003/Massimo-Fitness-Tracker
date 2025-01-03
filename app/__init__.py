bfe69749fbb9ed79dd0ab68e5a7cf68a102725d2
Ao Tian <aotian@berkeley.edu>
2025-01-03 17:27:09 +0800

    add with_credential

--- app/__init__.py
+++ app/__init__.py
@@ -29,7 +29,7 @@ def create_app():
     bcrypt.init_app(app)
     login_manager.init_app(app)
     Migrate(app, db)
-    CORS(app, resources={r"/*": {"origins": "https://massimo-frontend-7mbcpsyrs-aos-projects-2be27b28.vercel.app"}})
+    CORS(app, supports_credentials=True, resources={r"/*": {"origins": "https://massimo-frontend-7mbcpsyrs-aos-projects-2be27b28.vercel.app"}})
     
     app.config['SESSION_TYPE'] = 'filesystem'
     app.config['SESSION_COOKIE_SAMESITE'] = 'None'
