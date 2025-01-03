f54c5147098eca79c2c0bd8973c729c96793412f
Ao Tian <aotian@berkeley.edu>
2025-01-04 07:37:24 +0800

    mvckwlx

--- app/routes.py
+++ app/routes.py
@@ -9,7 +9,7 @@ from app.services import (
     generate_llm_recommendation
 )
 from app import bcrypt, db
-from flask_cors import cross_origin
+from flask_cors import cross_origin, CORS
 from datetime import datetime
 from llama_index.llms.openai import OpenAI
 from llama_index.core.llms import ChatMessage, MessageRole
@@ -21,7 +21,10 @@ import os
 main = Blueprint('main', __name__)
 
 @main.route('/register', methods=['POST'])
-# @cross_origin(supports_credentials=True, origins=["http://localhost:3000"])
+# @cross_origin(supports_credentials=True, origins=[
+    # "localhost:3000", 
+    # "https://massimo-frontend-4ot1an-aos-projects-2be27b28.vercel.app"
+    # ])
 def register():
     from app import db
     data = request.get_json()
@@ -34,7 +37,10 @@ def register():
     return jsonify({'message': 'User registered successfully'}), 201
 
 # @main.route('/login', methods=['POST'])
-# # @cross_origin(supports_credentials=True, origins=["http://localhost:3000"])
+# # @cross_origin(supports_credentials=True, origins=[
+    # "localhost:3000", 
+    # "https://massimo-frontend-4ot1an-aos-projects-2be27b28.vercel.app"
+    # ])
 # def login():
 #     data = request.get_json()
 #     user = User.query.filter_by(email=data['email']).first() #TODO Alex: is filtering by email enough?
@@ -90,14 +96,20 @@ def update_user():
 
 
 @main.route('/logout', methods=['POST'])
-@cross_origin(supports_credentials=True, origins=["massimo-frontend-4ot1an-aos-projects-2be27b28.vercel.app"])
+@cross_origin(supports_credentials=True, origins=[
+    "localhost:3000", 
+    "https://massimo-frontend-4ot1an-aos-projects-2be27b28.vercel.app"
+    ])
 @login_required
 def logout():
     logout_user()
     return jsonify({'message': 'Logged out successfully'}), 200
 
 @main.route('/add_aerobic', methods=['POST'])
-# @cross_origin(supports_credentials=True, origins=["http://localhost:3000"])
+# @cross_origin(supports_credentials=True, origins=[
+    # "localhost:3000", 
+    # "https://massimo-frontend-4ot1an-aos-projects-2be27b28.vercel.app"
+    # ])
 @login_required
 def add_aerobic():
     data = request.get_json()
@@ -139,7 +151,10 @@ def delete_aerobic(aerobic_id):
     return jsonify({'message': 'Session not found'}), 404
 
 @main.route('/add_strength', methods=['POST'])
-# @cross_origin(supports_credentials=True, origins=["http://localhost:3000"])
+# @cross_origin(supports_credentials=True, origins=[
+    # "localhost:3000", 
+    # "https://massimo-frontend-4ot1an-aos-projects-2be27b28.vercel.app"
+    # ])
 @login_required
 def add_strength():
     data = request.get_json()
@@ -147,7 +162,10 @@ def add_strength():
     return jsonify({'message': 'Strength training added', 'id': strength_session.id}), 201
 
 @main.route('/get_strength', methods=['GET'])
-@cross_origin(supports_credentials=True, origins=["http://localhost:3000"])
+@cross_origin(supports_credentials=True, origins=[
+    "localhost:3000", 
+    "https://massimo-frontend-4ot1an-aos-projects-2be27b28.vercel.app"
+    ])
 @login_required
 def get_strength():
     sessions = get_strength_training(current_user.id)
@@ -155,7 +173,10 @@ def get_strength():
     return jsonify(result), 200
 
 @main.route('/update_strength/<int:strength_id>', methods=['PUT'])
-@cross_origin(supports_credentials=True, origins=["http://localhost:3000"])
+@cross_origin(supports_credentials=True, origins=[
+    "localhost:3000", 
+    "https://massimo-frontend-4ot1an-aos-projects-2be27b28.vercel.app"
+    ])
 @login_required
 def update_strength(strength_id):
     updates = request.get_json()
@@ -165,7 +186,11 @@ def update_strength(strength_id):
     return jsonify({'message': 'Session not found'}), 404
 
 @main.route('/delete_strength/<int:strength_id>', methods=['DELETE'])
-@cross_origin(supports_credentials=True, origins=["http://localhost:3000"])
+@cross_origin(supports_credentials=True, origins=[
+    "localhost:3000", 
+    "https://massimo-frontend-4ot1an-aos-projects-2be27b28.vercel.app"
+    ])
+
 @login_required
 def delete_strength(strength_id):
     success = delete_strength_training(strength_id)
