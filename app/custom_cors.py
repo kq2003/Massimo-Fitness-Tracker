2fc5ed83a821ede36555a4faa44a2b156e3aa98a
Ao Tian <aotian@berkeley.edu>
2025-01-04 11:05:45 +0800

    update cors

--- app/custom_cors.py
+++ app/custom_cors.py
@@ -5,6 +5,7 @@ ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "https://massimo-frontend-7mbcpsyrs-aos-projects-2be27b28.vercel.app",
     "https://massimo-frontend-4ot1an-aos-projects-2be27b28.vercel.app",
+    "https://massimo-frontend-aos-projects-2be27b28.vercel.app",
     "https://massimo-frontend.vercel.app"
 ]
 
